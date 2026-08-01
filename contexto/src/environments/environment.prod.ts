ec0d1c3e1ab0286d53eb32b0a8a150c96a5a6f7e
secretary-tools-app <secretary.tools.app@gmail.com>
2026-08-01 16:07:11 -0300

    jsuaauidhsduhsadu

--- contexto/src/environments/environment.prod.ts
+++ contexto/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://atas-app-api-production.up.railway.app:8080/api',
+  apiUrl: 'https://atas-app-api-production.up.railway.app/api',
 };
